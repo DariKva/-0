bddb0cff775efc44915c532ce7cebd3c74b275bb
DariKva <daryada64@gmail.com>
2023-07-04 15:31:51 +0300

    Красивый график

---
+++ hw3.py
@@ -0,0 +1,35 @@
+import random as rng
+import matplotlib.pyplot as plt
+import numpy as np
+
+Max = 100
+Length = 50000
+
+
+def gaussgen(low, up):
+    s = low
+    for i in range(up):
+        s += rng.randint(0, 1)
+    return s
+
+
+def createDist(data):
+    dist = np.zeros(Max)
+    for n in data:
+        dist[n] = dist[n] + 1
+    for i in range(len(dist)):
+        dist[i] = dist[i] / len(data)
+    return dist
+
+
+data = np.array([gaussgen(0, Max) for i in range(Length)])
+# print(data)
+dist = createDist(data)
+# print(dist)
+fig, ax = plt.subplots()
+ax.bar(np.arange(0, Max), dist, width=0.7, color='pink')        
+ax.plot(np.arange(0, Max), dist,color='gray', marker='o', markersize=2)
+ax.set_title('Normal distribution')
+ax.set_xlabel('axis x')
+ax.set_ylabel('axis y')
+plt.show()
